1e54ecf866e7cecea1a2e9d619f5867dc6a9f3aa
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2022-07-23 22:48:21 -0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Title, Popover, Text, Button } from '@mantine/core';
+import { Title, Popover, Text } from '@mantine/core';
 import { Question, Info } from 'phosphor-react';
 import Logo from '../assets/logo.png';
 
@@ -7,7 +7,7 @@ const Header = () => {
   const [opened, setOpened] = useState(false);
 
   return (
-    <div className="h-14 flex items-center bg-white text-black border border-solid border-ft-grey bg-[#e7e8ea] p-2">
+    <div className="h-14 flex items-center text-black border border-solid border-ft-grey bg-[#e7e8ea] p-2">
       <img src={Logo} alt="logo" className="h-10 w-10" />
       <Title className="ml-2" order={1}>
         Fast Track
@@ -33,7 +33,7 @@ const Header = () => {
         position="bottom-end"
         withArrow
       >
-        <div className="flex flex-row ">
+        <div className="flex flex-row">
           <Info size={26} color="#DA0D2A" weight="fill" />
           <Text className="ml-2 w-5/6" size="sm">
             Enter a starting and ending location and stops in between and Fast
